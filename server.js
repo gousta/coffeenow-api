29d77090bf00357304df9c277df9ffcdd4aa19e5
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-07-07 21:43:27 +0300

    Removed node_modules from git

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const shops = require('./shops.json')
 const cfg = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  mongoDBConnectionString: process.env.MONGODB_URI || ''
 }
 
 // CORS
@@ -30,6 +32,11 @@ app.use((req, res, next) => {
   next()
 });
 
+app.use((req, res, next) => {
+  console.info(`[HTTP][${req.method}] ${req.originalUrl}`)
+  next()
+})
+
 // ROUTES
 app.get('/', (req, res) => {
   res.responseWithData()
@@ -47,7 +54,17 @@ app.post('/order', (req, res) => {
   res.responseWithData(req.body);
 })
 
+// MONGO
+mongoose
+  .connect(cfg.mongoDBConnectionString)
+  .then(() => {
+    console.info(`[MONGODB][CONNECT][OK] @ ${cfg.mongoDBConnectionString}`)
+  })
+  .catch((err) => {
+    console.error('[MONGODB][CONNECT][ERROR] @ ', err.message)
+  })
+
 // Start Server
 app.listen(cfg.port, () => {
-  console.info(`[CN][SERVER] is running on port ${cfg.port}`)
+  console.info(`[SERVER][OK] @ ${cfg.port}`)
 })
