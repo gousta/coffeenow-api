e3f605ad26c0908ed6efc3f723cb175513decf3d
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-07-07 16:19:41 +0300

    Bootstrap

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+const shops = require('./shops.json')
+
+const cfg = {
+  port: process.env.PORT || 3000
+}
+
+app.use((req, res, next) => {
+  res.responseWithError = (error, data) => {
+    return res.json({status: 'error', error: error, data: data})
+  }
+
+  res.responseWithData = (data) => {
+    return res.json({status: 'ok', data: data})
+  }
+
+  next()
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.responseWithData()
+})
+
+app.get('/shops', (req, res) => {
+  res.responseWithData(shops)
+})
+
+app.post('/order', (req, res) => {
+  const { shopId, productId} = req.body;
+
+  console.log(shopId, productId);
+})
+
+// Start Server
+app.listen(cfg.port, () => {
+  console.info(`[CN][SERVER] is running on port ${cfg.port}`)
+})
